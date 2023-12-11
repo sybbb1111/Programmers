672f9a62393bcf1da378dc8220938577e0c1b508
sybbb1111 <131143199+sybbb1111@users.noreply.github.com>
2023-12-11 23:29:52 +0900

    [level unrated] Title: qr code, Time: 8.86 ms, Memory: 92.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181903. qr code/qr code.java
+++ 프로그래머스/unrated/181903. qr code/qr code.java
@@ -1,13 +1,13 @@
 class Solution {
     public String solution(int q, int r, String code) {
         String answer = "";
-        String[] arr = code.split("");
                 
         for(int i=0; i<code.length();i++){
             if(i%q==r){
-                answer += arr[i];
+                answer += code.charAt(i);
             }
         }
+        
         return answer;
     }
 }
