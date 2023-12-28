0fc3e1f214da7125648268052be864a547e4f314
sybbb1111 <131143199+sybbb1111@users.noreply.github.com>
2023-12-28 22:57:35 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.15 ms, Memory: 82.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -6,7 +6,9 @@ class Solution {
         
         for(int i=0; i<n;i++){
             for(int j=0;j<n;j++){
-                answer[i][j] = i==j ? 1 : 0;
+                if(i==j) { 
+                    answer[i][j] = 1;
+                }
             }
         }
         return answer;
